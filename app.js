0419e1c3ec1f2e81f2d30fb3082d495980a5f1f5
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2018-04-25 17:56:08 -0300

    adicionado linha stela

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use('/stela', indexRouter);
 app.use('/users', usersRouter);
 
 // error handler

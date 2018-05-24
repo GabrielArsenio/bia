90e2ffbae452f6aed03165084ffe682352d03b80
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2018-05-24 00:20:42 -0300

    Criados componentes dos cadastros

---
+++ app/src/components/cadastros/Ameaca.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-dialog v-model="cadastroAmeacas" max-width="500px">
+        <v-card>
+            <v-card-title class="grey lighten-4 py-4 title">
+                Nova ameaça
+            </v-card-title>
+            <v-container grid-list-sm class="pa-4">
+                <v-layout row wrap>
+
+                    <v-flex xs3>
+                        <v-text-field placeholder="Código"></v-text-field>
+                    </v-flex>
+
+                    <v-flex xs12>
+                        <v-text-field placeholder="Descrição"></v-text-field>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="cadastroAmeacas = false">Cancelar</v-btn>
+                <v-btn flat @click="cadastroAmeacas = false">Salvar</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
---
+++ app/src/components/cadastros/Nivel.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-dialog v-model="cadastroNiveisCricitidade" max-width="500px">
+        <v-card>
+            <v-card-title class="grey lighten-4 py-4 title">
+                Novo nível de criticidade
+            </v-card-title>
+            <v-container grid-list-sm class="pa-4">
+                <v-layout row wrap>
+
+                    <v-flex xs3>
+                        <v-text-field placeholder="Código"></v-text-field>
+                    </v-flex>
+
+                    <v-flex xs12>
+                        <v-text-field placeholder="Descrição"></v-text-field>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="cadastroNiveisCricitidade = false">Cancelar</v-btn>
+                <v-btn flat @click="cadastroNiveisCricitidade = false">Salvar</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>

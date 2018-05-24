9417a2241696091a994ad87a3bb0b446970083c2
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2018-05-24 00:19:39 -0300

    Adicionados headers de access control apenas para desenvolvimento

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -16,6 +16,12 @@ router.param('resource', function (req, res, next) {
     next();
 });
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 router.get('/:resource', function (req, res, next) {
     let collectionModel = createCollectionModel(req.params.resource);
 

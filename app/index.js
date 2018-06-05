7e3d704031e622213d528d0dd0b26f805c2756c9
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2018-06-04 23:31:28 -0300

    Unificado os projetos do frontend e backend

--- app/index.js
+++ app/index.js
@@ -1,21 +1,21 @@
-import Vue from 'vue/dist/vue'
+import Vue from 'vue/dist/vue';
 
-import VueRouter from 'vue-router/dist/vue-router'
-import { routes } from './routes'
+import VueRouter from 'vue-router/dist/vue-router';
+import { routes } from './routes';
 
 import VueResource from 'vue-resource/dist/vue-resource';
 
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-import App from './src/App.vue'
+import App from './src/App.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000/api';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 const router = new VueRouter({ routes });
 

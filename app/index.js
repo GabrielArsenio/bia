95334d8d8555906433d6dafea690621835a6464d
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2018-06-06 17:27:47 -0300

    Ajustadas importações e filtrado ambiente de produção para o root API

--- app/index.js
+++ app/index.js
@@ -1,21 +1,18 @@
 import Vue from 'vue/dist/vue';
-
 import VueRouter from 'vue-router/dist/vue-router';
-import { routes } from './routes';
-
 import VueResource from 'vue-resource/dist/vue-resource';
-
 import Vuetify from 'vuetify';
+import App from './src/App.vue';
+import { routes } from './routes';
 import 'vuetify/dist/vuetify.min.css';
 
-import App from './src/App.vue';
-
 Vue.use(VueRouter);
-
 Vue.use(VueResource);
-Vue.http.options.root = 'http://localhost:3000/api';
-
 Vue.use(Vuetify);
+
+if (process.env.NODE_ENV !== 'production') {
+    Vue.http.options.root = 'http://localhost:3000/api';
+}
 
 const router = new VueRouter({ routes });
 
@@ -24,5 +21,3 @@ new Vue({
     router,
     render: h => h(App)
 });
-
-console.log(process.env.NODE_ENV)

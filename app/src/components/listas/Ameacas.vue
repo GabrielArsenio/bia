4c59a73bd36a3fe7904bffc2e2a8d8762d921ced
GabrielArsenio <gabrielarsenioo@gmail.com>
2018-06-20 21:15:44 -0300

    Retirado botão flutuante

--- app/src/components/listas/Ameacas.vue
+++ app/src/components/listas/Ameacas.vue
@@ -6,10 +6,12 @@
             <v-text-field v-model="search" append-icon="search" label="Pesquisar ameaças" single-line hide-details></v-text-field>
         </v-card-title>
 
-        <v-data-table hide-actions :headers="headers" :items="items" :search="search">
+        <v-btn slot="activator" color="primary" dark class="mb-2" @click.stop="create">Novo</v-btn>
+
+        <v-data-table hide-actions fix-header :headers="headers" :items="items" :search="search">
             <template slot="items" slot-scope="props">
                 <td>{{ props.item.descricao }}</td>
-                <td class="justify-center layout px-0">
+                <td class="text-xs-right">
                     <v-btn icon class="mx-0" @click="edit(props.item)">
                         <v-icon color="teal">edit</v-icon>
                     </v-btn>
@@ -19,12 +21,6 @@
                 </td>
             </template>
         </v-data-table>
-
-        <v-btn fab bottom right color="primary" dark fixed @click.stop="create">
-            <v-icon>
-                add
-            </v-icon>
-        </v-btn>
 
         <cadastro-ameaca :document="document" @cancel="document = false" @save="onSave"></cadastro-ameaca>
 
@@ -62,7 +58,7 @@
                 search: '',
                 headers: [
                     { text: 'Descrição', value: 'descricao' },
-                    { text: '', value: 'descricao', sortable: false, align: 'right' }
+                    { text: 'Ações', value: 'descricao', sortable: false, align: 'right' }
                 ],
                 items: []
             }

757c04b2768db39e61d8ee48ae0dbf463ea70c7c
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-19 00:48:42 +0100

    Applied mixins

--- app/src/components/cadastros/CadastroAmeaca.vue
+++ app/src/components/cadastros/CadastroAmeaca.vue
@@ -1,75 +1,48 @@
 <template>
     <v-dialog v-model="isOpen" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">
+                Nova ameaça
+            </v-btn>
+        </template>
         <v-card>
             <v-card-title class="grey lighten-4 py-4 title">
-                {{ tempDocument._id ? 'Editar' : 'Nova' }} ameaça
+                {{ form._id ? 'Editar' : 'Nova' }} ameaça
             </v-card-title>
             <v-container grid-list-sm class="pa-4">
                 <v-layout row wrap>
                     <v-flex xs3>
-                        <v-text-field
-                            label="Código"
-                            v-model="tempDocument._id"
-                            disabled
-                        ></v-text-field>
+                        <v-text-field label="Código" v-model="form._id" disabled></v-text-field>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-text-field
-                            label="Descrição"
-                            v-model="tempDocument.descricao"
-                            name="descricao"
-                            v-validate="'required'"
-                            :error="errors.has('descricao')"
-                        ></v-text-field>
+                        <v-text-field label="Descrição" v-model="form.descricao" name="descricao"
+                            v-validate="'required'" :error="errors.has('descricao')"></v-text-field>
                     </v-flex>
                 </v-layout>
             </v-container>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="cancel()">Cancelar</v-btn>
-                <v-btn flat color="primary" @click="save()">Salvar</v-btn>
+                <v-btn @click="cancel()">Cancelar</v-btn>
+                <v-btn color="primary" @click="save()">Salvar</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
+import CrudFormMixin from '../../mixins/crud-form.mixin';
 
-    export default {
-        props: {
-            document: {
-                required: true
-            }
-        },
-        data() {
-            return {
-                isOpen: false,
-                tempDocument: {}
-            }
-        },
-        watch: {
-            document: function (newVal, oldVal) {
-                if (!this.document) {
-                    this.isOpen = false;
-                }
-
-                if (typeof this.document === 'object') {
-                    this.isOpen = true
-                    this.tempDocument = Object.assign({}, this.document)
-                }
-            }
-        },
-        methods: {
-            cancel() {
-                this.$emit('cancel')
-            },
-            save() {
-                new Service(this.$resource('api/ameacas{/id}'))
-                    .save(this.tempDocument)
-                    .then(res => this.$emit('save', res.body || this.tempDocument))
-            }
+export default {
+    mixins: [CrudFormMixin],
+    props: {
+        document: {
+            required: true
         }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/ameacas{/id}'))
     }
+}
 </script>
--- app/src/components/listas/Ameacas.vue
+++ app/src/components/listas/Ameacas.vue
@@ -1,115 +1,57 @@
 <template>
-    <div>
-        <v-card-title>
-            Ameaças
-            <v-spacer></v-spacer>
-            <v-text-field v-model="search" append-icon="search" label="Pesquisar ameaças" single-line hide-details></v-text-field>
-        </v-card-title>
+    <v-container fluid>
 
-        <v-btn slot="activator" color="primary" dark class="mb-2" @click.stop="create">Novo</v-btn>
+        <v-data-table hide-default-footer fix-header :headers="headers" :items="items" :search="search">
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>Ameaças</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>
+                    <v-text-field v-model="search" append-icon="search" label="Pesquisar" single-line hide-details>
+                    </v-text-field>
+                    <v-spacer></v-spacer>
+                    <CadastroAmeaca :document="itemEditing" @cancel="itemEditing = null" @save="onSave">
+                    </CadastroAmeaca>
+                </v-toolbar>
+            </template>
 
-        <v-data-table hide-actions fix-header :headers="headers" :items="items" :search="search">
-            <template slot="items" slot-scope="props">
-                <td>{{ props.item.descricao }}</td>
-                <td class="text-xs-right">
-                    <v-btn icon class="mx-0" @click="edit(props.item)">
-                        <v-icon color="teal">edit</v-icon>
-                    </v-btn>
-                    <v-btn icon class="mx-0" @click="remove(props.item)">
-                        <v-icon color="pink">delete</v-icon>
-                    </v-btn>
-                </td>
+            <template v-slot:item.actions="{ item }">
+                <v-icon small class="mr-2" @click="edit(item)">
+                    edit
+                </v-icon>
+                <v-icon small @click="remove(item)">
+                    delete
+                </v-icon>
             </template>
         </v-data-table>
 
-        <CadastroAmeaca :document="document" @cancel="document = false" @save="onSave"></CadastroAmeaca>
-
-        <DialogConfirmRemove :active="documentRemoving" @cancel="documentRemoving = false" @remove="onRemove"></DialogConfirmRemove>
-
-        <v-snackbar :timeout="6000" :bottom="true" v-model="alertSaved">
-            Registro salvo com sucesso!
-            <v-btn flat color="white" @click.native="alertSaved = false">Fechar</v-btn>
-        </v-snackbar>
-
-        <v-snackbar :timeout="6000" :bottom="true" v-model="alertRemoved">
-            Registro removido com sucesso!
-            <v-btn flat color="pink" @click.native="alertRemoved = false">Fechar</v-btn>
-        </v-snackbar>
-    </div>
+        <DialogConfirmRemove :active="itemRemoving" @cancel="itemRemoving = null" @remove="onRemove">
+        </DialogConfirmRemove>
+    </v-container>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
-    import DialogConfirmRemove from '../shared/DialogConfirmRemove'
-    import CadastroAmeaca from '../cadastros/CadastroAmeaca'
+import { Service } from '../../domain/Service'
+import DialogConfirmRemove from '../shared/DialogConfirmRemove'
+import CadastroAmeaca from '../cadastros/CadastroAmeaca'
+import CrudListMixin from '../../mixins/crud-list.mixin'
 
-    export default {
-        name: 'Ameacas',
-        components: {
-            DialogConfirmRemove,
-            CadastroAmeaca
-        },
-        data() {
-            return {
-                alertSaved: false,
-                alertRemoved: false,
-                document: false,
-                documentRemoving: false,
-                search: '',
-                headers: [
-                    { text: 'Descrição', value: 'descricao' },
-                    { text: 'Ações', value: 'descricao', sortable: false, align: 'right' }
-                ],
-                items: []
-            }
-        },
-        created() {
-            this.service = new Service(this.$resource('api/ameacas{/id}'));
-
-            this.service
-                .findAll()
-                .then(items => this.items = items);
-        },
-        methods: {
-            create() {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = {}
-            },
-            edit(document) {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = document
-            },
-            remove(document) {
-                if (this.documentRemoving) {
-                    this.documentRemoving = false
-                }
-                this.documentRemoving = document
-            },
-            onSave(newDocument) {
-                this.alertSaved = true
-                if (!this.document._id) {
-                    this.items.push(newDocument)
-                } else {
-                    let indice = this.items.indexOf(this.document)
-                    this.items.splice(indice, 1)
-                    this.items.push(newDocument)
-                }
-                this.document = false
-            },
-            onRemove() {
-                this.service
-                    .remove(this.documentRemoving._id)
-                    .then(() => {
-                        let indice = this.items.indexOf(this.documentRemoving);
-                        this.items.splice(indice, 1);
-                        this.alertRemoved = true;
-                        this.documentRemoving = false;
-                    });
-            }
+export default {
+    name: 'Ameacas',
+    mixins: [CrudListMixin],
+    components: {
+        DialogConfirmRemove,
+        CadastroAmeaca
+    },
+    data() {
+        return {
+            headers: [
+                { text: 'Descrição', value: 'descricao' },
+                { text: 'Ações', value: 'actions', sortable: false, align: 'right' }
+            ]
         }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/ameacas{/id}'));
     }
+}
 </script>

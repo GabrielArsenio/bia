43e7f81996d40d220438f5559048c56f118480e5
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-18 19:40:56 +0100

    Refactor Criticity Levels

--- app/src/components/cadastros/CadastroNivel.vue
+++ app/src/components/cadastros/CadastroNivel.vue
@@ -1,5 +1,10 @@
 <template>
     <v-dialog v-model="isOpen" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">
+                Novo nível
+            </v-btn>
+        </template>
         <v-card>
             <v-card-title class="grey lighten-4 py-4 title">
                 {{ tempDocument._id ? 'Editar' : 'Novo' }} nível de criticidade
@@ -17,48 +22,50 @@
             </v-container>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="cancel()">Cancelar</v-btn>
-                <v-btn flat color="primary" @click="save()">Salvar</v-btn>
+                <v-btn @click="cancel()">Cancelar</v-btn>
+                <v-btn color="primary" @click="save()">Salvar</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
 
-    export default {
-        props: {
-            document: {
-                required: true
+export default {
+    props: {
+        document: {
+            required: true
+        }
+    },
+    data() {
+        return {
+            isOpen: false,
+            tempDocument: {}
+        }
+    },
+    watch: {
+        document: function (newVal, oldVal) {
+            if (!this.document) {
+                this.isOpen = false;
             }
-        },
-        data() {
-            return {
-                isOpen: false,
-                tempDocument: {}
-            }
-        },
-        watch: {
-            document: function (newVal, oldVal) {
-                if (!this.document) {
-                    this.isOpen = false;
-                }
-                if (typeof this.document === 'object') {
-                    this.isOpen = true
-                    this.tempDocument = Object.assign({}, this.document)
-                }
-            }
-        },
-        methods: {
-            cancel() {
-                this.$emit('cancel')
-            },
-            save() {
-                new Service(this.$resource('api/niveis{/id}'))
-                    .save(this.tempDocument)
-                    .then(res => this.$emit('save', res.body || this.tempDocument))
+            if (typeof this.document === 'object') {
+                this.isOpen = true
+                this.tempDocument = Object.assign({}, this.document)
             }
         }
+    },
+    methods: {
+        cancel() {
+            this.$emit('cancel')
+            this.isOpen = false;
+        },
+        save() {
+            new Service(this.$resource('api/niveis{/id}'))
+                .save(this.tempDocument)
+                .then(res => this.$emit('save', res.body || this.tempDocument))
+            this.isOpen = false;
+        }
     }
+}
 </script>
--- app/src/components/listas/Niveis.vue
+++ app/src/components/listas/Niveis.vue
@@ -1,115 +1,102 @@
 <template>
-    <div>
-        <v-card-title>
-            Níveis de criticidade
-            <v-spacer></v-spacer>
-            <v-text-field v-model="search" append-icon="search" label="Pesquisar níveis de criticidade" single-line hide-details></v-text-field>
-        </v-card-title>
+    <v-container fluid>
 
-        <v-btn slot="activator" color="primary" dark class="mb-2" @click.stop="create">Novo</v-btn>
-
-        <v-data-table hide-actions :headers="headers" :items="items" :search="search">
-            <template slot="items" slot-scope="props">
-                <td>{{ props.item.descricao }}</td>
-                <td class="justify-center layout px-0">
-                    <v-btn icon class="mx-0" @click="edit(props.item)">
-                        <v-icon color="teal">edit</v-icon>
-                    </v-btn>
-                    <v-btn icon class="mx-0" @click="remove(props.item)">
-                        <v-icon color="pink">delete</v-icon>
-                    </v-btn>
-                </td>
+        <v-data-table hide-default-footer fix-header :headers="headers" :items="items" :search="search">
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>Níveis de criticidade</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>
+                    <v-text-field v-model="search" append-icon="search" label="Pesquisar" single-line hide-details>
+                    </v-text-field>
+                    <v-spacer></v-spacer>
+                    <CadastroNivel :document="document" @cancel="document = false" @save="onSave"></CadastroNivel>
+                </v-toolbar>
             </template>
+
+            <template v-slot:item.actions="{ item }">
+                <v-icon small class="mr-2" @click="edit(item)">
+                    edit
+                </v-icon>
+                <v-icon small @click="remove(item)">
+                    delete
+                </v-icon>
+            </template>
+
         </v-data-table>
 
-        <CadastroNivel :document="document" @cancel="document = false" @save="onSave"></CadastroNivel>
-
-        <DialogConfirmRemove :active="documentRemoving" @cancel="documentRemoving = false" @remove="onRemove"></DialogConfirmRemove>
-
-        <v-snackbar :timeout="6000" :bottom="true" v-model="alertSaved">
-            Registro salvo com sucesso!
-            <v-btn flat color="white" @click.native="alertSaved = false">Fechar</v-btn>
-        </v-snackbar>
-
-        <v-snackbar :timeout="6000" :bottom="true" v-model="alertRemoved">
-            Registro removido com sucesso!
-            <v-btn flat color="pink" @click.native="alertRemoved = false">Fechar</v-btn>
-        </v-snackbar>
-    </div>
+        <DialogConfirmRemove :active="documentRemoving" @cancel="documentRemoving = false" @remove="onRemove">
+        </DialogConfirmRemove>
+    </v-container>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
-    import DialogConfirmRemove from '../shared/DialogConfirmRemove'
-    import CadastroNivel from '../cadastros/CadastroNivel'
+import { Service } from '../../domain/Service'
+import DialogConfirmRemove from '../shared/DialogConfirmRemove'
+import CadastroNivel from '../cadastros/CadastroNivel'
 
-    export default {
-        name: 'Niveis',
-        components: {
-            DialogConfirmRemove,
-            CadastroNivel
-        },
-        data() {
-            return {
-                alertSaved: false,
-                alertRemoved: false,
-                document: null,
-                documentRemoving: null,
-                search: '',
-                headers: [
-                    { text: 'Descrição', value: 'descricao' },
-                    { text: '', value: 'descricao', sortable: false, align: 'right' }
-                ],
-                items: []
-            }
-        },
-        created() {
-            this.service = new Service(this.$resource('api/niveis{/id}'));
+export default {
+    name: 'Niveis',
+    components: {
+        DialogConfirmRemove,
+        CadastroNivel
+    },
+    data() {
+        return {
+            document: null,
+            documentRemoving: null,
+            search: '',
+            headers: [
+                { text: 'Descrição', value: 'descricao' },
+                { text: 'Ações', value: 'actions', sortable: false, align: 'right' }
+            ],
+            items: []
+        }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/niveis{/id}'));
 
-            this.service
-                .findAll()
-                .then(items => this.items = items);
-        },
-        methods: {
-            create() {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = {}
-            },
-            edit(document) {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = document
-            },
-            remove(document) {
-                if (this.documentRemoving) {
-                    this.documentRemoving = false
-                }
-                this.documentRemoving = document
-            },
-            onSave(newDocument) {
-                this.alertSaved = true
-                if (!this.document._id) {
-                    this.items.push(newDocument)
-                } else {
-                    let indice = this.items.indexOf(this.document)
-                    this.items.splice(indice, 1)
-                    this.items.push(newDocument)
-                }
+        this.service
+            .findAll()
+            .then(items => this.items = items);
+    },
+    methods: {
+        create() {
+            if (this.document) {
                 this.document = false
-            },
-            onRemove() {
-                this.service
-                    .remove(this.documentRemoving._id)
-                    .then(() => {
-                        let indice = this.items.indexOf(this.documentRemoving);
-                        this.items.splice(indice, 1);
-                        this.alertRemoved = true;
-                        this.documentRemoving = false;
-                    });
             }
+            this.document = {}
+        },
+        edit(document) {
+            if (this.document) {
+                this.document = false
+            }
+            this.document = document
+        },
+        remove(document) {
+            if (this.documentRemoving) {
+                this.documentRemoving = false
+            }
+            this.documentRemoving = document
+        },
+        onSave(newDocument) {
+            if (!this.document._id) {
+                this.items.push(newDocument)
+            } else {
+                let indice = this.items.indexOf(this.document)
+                this.items.splice(indice, 1)
+                this.items.push(newDocument)
+            }
+            this.document = false
+        },
+        onRemove() {
+            this.service
+                .remove(this.documentRemoving._id)
+                .then(() => {
+                    let indice = this.items.indexOf(this.documentRemoving);
+                    this.items.splice(indice, 1);
+                    this.documentRemoving = false;
+                });
         }
     }
+}
 </script>

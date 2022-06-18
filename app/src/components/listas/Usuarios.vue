e3414b8bf3546650735a844d18cc8d0d82418023
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-18 11:23:09 +0100

    Fixed layout of users list

--- app/src/components/listas/Usuarios.vue
+++ app/src/components/listas/Usuarios.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
+    <v-container fluid>
         <v-card-title>
             Usuários
             <v-spacer></v-spacer>
-            <v-text-field v-model="search" append-icon="search" label="Pesquisar usuários" single-line hide-details></v-text-field>
+            <v-text-field v-model="search" append-icon="search" label="Pesquisar usuários" single-line hide-details>
+            </v-text-field>
         </v-card-title>
 
         <v-btn slot="activator" color="primary" dark class="mb-2" @click.stop="create">Novo</v-btn>
@@ -25,7 +26,8 @@
 
         <CadastroUsuario :document="document" @cancel="document = false" @save="onSave"></CadastroUsuario>
 
-        <DialogConfirmRemove :active="documentRemoving" @cancel="documentRemoving = false" @remove="onRemove"></DialogConfirmRemove>
+        <DialogConfirmRemove :active="documentRemoving" @cancel="documentRemoving = false" @remove="onRemove">
+        </DialogConfirmRemove>
 
         <v-snackbar :timeout="6000" :bottom="true" v-model="alertSaved">
             Registro salvo com sucesso!
@@ -36,82 +38,82 @@
             Registro removido com sucesso!
             <v-btn text color="pink" @click.native="alertRemoved = false">Fechar</v-btn>
         </v-snackbar>
-    </div>
+    </v-container>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
-    import DialogConfirmRemove from '../shared/DialogConfirmRemove'
-    import CadastroUsuario from '../cadastros/CadastroUsuario'
+import { Service } from '../../domain/Service'
+import DialogConfirmRemove from '../shared/DialogConfirmRemove'
+import CadastroUsuario from '../cadastros/CadastroUsuario'
 
-    export default {
-        name: 'Usuarios',
-        components: {
-            DialogConfirmRemove,
-            CadastroUsuario
-        },
-        data() {
-            return {
-                alertSaved: false,
-                alertRemoved: false,
-                document: false,
-                documentRemoving: false,
-                search: '',
-                headers: [
-                    { text: 'Nome', value: 'nome' },
-                    { text: 'Login', value: 'login' },
-                    { text: 'Ações', value: 'descricao', sortable: false, align: 'right' }
-                ],
-                items: []
-            }
-        },
-        created() {
-            this.service = new Service(this.$resource('api/usuarios{/id}'));
+export default {
+    name: 'Usuarios',
+    components: {
+        DialogConfirmRemove,
+        CadastroUsuario
+    },
+    data() {
+        return {
+            alertSaved: false,
+            alertRemoved: false,
+            document: false,
+            documentRemoving: false,
+            search: '',
+            headers: [
+                { text: 'Nome', value: 'nome' },
+                { text: 'Login', value: 'login' },
+                { text: 'Ações', value: 'descricao', sortable: false, align: 'right' }
+            ],
+            items: []
+        }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/usuarios{/id}'));
 
-            this.service
-                .findAll()
-                .then(items => this.items = items);
-        },
-        methods: {
-            create() {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = {}
-            },
-            edit(document) {
-                if (this.document) {
-                    this.document = false
-                }
-                this.document = document
-            },
-            remove(document) {
-                if (this.documentRemoving) {
-                    this.documentRemoving = false
-                }
-                this.documentRemoving = document
-            },
-            onSave(newDocument) {
-                this.alertSaved = true
-                if (!this.document._id) {
-                    this.items.push(newDocument)
-                } else {
-                    let indice = this.items.indexOf(this.document)
-                    this.items.splice(indice, 1)
-                    this.items.push(newDocument)
-                }
+        this.service
+            .findAll()
+            .then(items => this.items = items);
+    },
+    methods: {
+        create() {
+            if (this.document) {
                 this.document = false
-            },
-            onRemove() {
-                this.service
-                    .remove(this.documentRemoving._id)
-                    .then(() => {
-                        let indice = this.items.indexOf(this.documentRemoving);
-                        this.items.splice(indice, 1);
-                        this.alertRemoved = true;
-                        this.documentRemoving = false;
-                    });
             }
+            this.document = {}
+        },
+        edit(document) {
+            if (this.document) {
+                this.document = false
+            }
+            this.document = document
+        },
+        remove(document) {
+            if (this.documentRemoving) {
+                this.documentRemoving = false
+            }
+            this.documentRemoving = document
+        },
+        onSave(newDocument) {
+            this.alertSaved = true
+            if (!this.document._id) {
+                this.items.push(newDocument)
+            } else {
+                let indice = this.items.indexOf(this.document)
+                this.items.splice(indice, 1)
+                this.items.push(newDocument)
+            }
+            this.document = false
+        },
+        onRemove() {
+            this.service
+                .remove(this.documentRemoving._id)
+                .then(() => {
+                    let indice = this.items.indexOf(this.documentRemoving);
+                    this.items.splice(indice, 1);
+                    this.alertRemoved = true;
+                    this.documentRemoving = false;
+                });
         }
     }
+}
 </script>

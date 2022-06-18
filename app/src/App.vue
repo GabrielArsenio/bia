f0a84b884c21819a13e5934a5a7014fb272c24d1
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-18 18:33:47 +0100

    Added EventBus

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <v-app>
     <router-view></router-view>
+
+    <v-snackbar :timeout="6000" :bottom="true" v-model="snackbarSaved">
+      Registro salvo com sucesso!
+      <v-btn text color="white" @click.native="snackbarSaved = false">Fechar</v-btn>
+    </v-snackbar>
+
+    <v-snackbar :timeout="6000" :bottom="true" v-model="snackbarRemoved">
+      Registro removido com sucesso!
+      <v-btn text color="white" @click.native="snackbarRemoved = false">Fechar</v-btn>
+    </v-snackbar>
+
+    <v-snackbar :timeout="6000" :bottom="true" v-model="snackbarFindFailure">
+      Erro ao listar!
+      <v-btn text color="white" @click.native="snackbarFindFailure = false">Fechar</v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import EventBus from './event-bus';
+
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      snackbarSaved: false,
+      snackbarRemoved: false,
+      snackbarFindFailure: false
+    }
+  },
+  mounted() {
+    EventBus.$on('save-success', () => this.snackbarSaved = true)
+    EventBus.$on('remove-success', () => this.snackbarRemoved = true)
+    EventBus.$on('find-failure', () => this.snackbarFindFailure = true)
+  }
 };
 </script>
--- app/src/domain/Service.js
+++ app/src/domain/Service.js
@@ -1,32 +1,35 @@
+import EventBus from '../event-bus'
+
 export class Service {
 
     constructor(resource) {
         this._resource = resource;
     }
 
-    findAll(queryParams = {}) {
-        return this._resource
-            .query(queryParams)
-            .then(res => res.json(), err => {
-                console.log(err);
-                throw new Error('Não foi possível listar.');
-            });
-    }
-
-    remove(id) {
-        return this._resource
-            .delete({ id })
-            .then(null, err => {
-                console.log(err);
-                throw new Error('Não foi possível remover.');
-            });
-    }
-
-    save(document) {
-        if (document._id) {
-            return this._resource.update({ id: document._id }, document);
-        } else {
-            return this._resource.save(document);
+    async findAll(queryParams = {}) {
+        try {
+            const res = await this._resource.query(queryParams)
+            return res.json()
+        } catch (error) {
+            EventBus.$emit('find-failure');
         }
+    }
+
+    async remove(id) {
+        await this._resource.delete({ id })
+        EventBus.$emit('remove-success');
+    }
+
+    async save(document) {
+        let record = null;
+
+        if (document._id) {
+            record = await this._resource.update({ id: document._id }, document);
+        } else {
+            record = await this._resource.save(document);
+        }
+
+        EventBus.$emit('save-success');
+        return record
     }
 }

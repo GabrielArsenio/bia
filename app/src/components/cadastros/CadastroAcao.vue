178d43e214ff0d5505d1761826b33a04ce26e187
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-19 12:01:16 +0100

    Applied Mixins

--- app/src/components/cadastros/CadastroAcao.vue
+++ app/src/components/cadastros/CadastroAcao.vue
@@ -1,123 +1,70 @@
 <template>
     <v-dialog v-model="isOpen" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">
+                Nova ação
+            </v-btn>
+        </template>
         <v-card>
             <v-card-title class="grey lighten-4 py-4 title">
-                Novo indicativo de ação
+                {{ form._id ? 'Editar' : 'Novo' }} indicativo de ação
             </v-card-title>
             <v-container grid-list-sm class="pa-4">
                 <v-layout row wrap>
 
                     <v-flex xs3>
-                        <v-text-field
-                            label="Código"
-                            v-model="document._id"
-                            disabled
-                        ></v-text-field>
+                        <v-text-field label="Código" v-model="form._id" disabled></v-text-field>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-autocomplete 
-                            label="Processo" 
-                            v-model="tempDocument.processo"
-                            :items="processos" 
-                            item-text="descricao" 
-                            item-value="_id" 
-                        ></v-autocomplete>
+                        <v-autocomplete label="Processo" v-model="form.processo" :items="processos"
+                            item-text="descricao" item-value="_id"></v-autocomplete>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-autocomplete 
-                            label="Ameaças" 
-                            v-model="tempDocument.ameaca"
-                            :items="ameacas" 
-                            item-text="descricao" 
-                            item-value="_id" 
-                        ></v-autocomplete>
+                        <v-autocomplete label="Ameaças" v-model="form.ameaca" :items="ameacas" item-text="descricao"
+                            item-value="_id"></v-autocomplete>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-textarea
-                            label="Procedimento"
-                            v-model="tempDocument.procedimento"
-                        ></v-textarea>
+                        <v-textarea label="Procedimento" v-model="form.procedimento"></v-textarea>
                     </v-flex>
                 </v-layout>
             </v-container>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="cancel()">Cancelar</v-btn>
-                <v-btn flat color="primary" @click="save()">Salvar</v-btn>
+                <v-btn @click="cancel()">Cancelar</v-btn>
+                <v-btn color="primary" @click="save()">Salvar</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
+import CrudFormMixin from '../../mixins/crud-form.mixin';
 
-    export default {
-        props: {
-            document: {
-                required: true
-            }
-        },
-        data() {
-            return {
-                isOpen: false,
-                tempDocument: {},
-                processos: [],
-                ameacas: []
-            }
-        },
-        watch: {
-            document: function (newVal, oldVal) {
-                if (!this.document) {
-                    this.isOpen = false;
-                }
-                if (typeof this.document === 'object') {
-                    this.isOpen = true
-                    this.tempDocument = Object.assign({}, this.document)
-
-                    if (this.tempDocument.processo) {
-                        this.tempDocument.processo = this.tempDocument.processo._id
-                    }
-
-                    if (this.tempDocument.ameaca) {
-                        this.tempDocument.ameaca = this.tempDocument.ameaca._id
-                    }
-
-                    this.getProcessos()
-                    this.getAmeacas()
-                }
-            }
-        },
-        methods: {
-            cancel() {
-                this.$emit('cancel')
-            },
-            save() {
-                new Service(this.$resource('api/acoes{/id}'))
-                    .save(this.tempDocument)
-                    .then((res) => {
-                        let selectedProcesso = this.processos.find(processo => processo._id === this.tempDocument.processo);
-                        this.tempDocument.processo = selectedProcesso;
-
-                        let selectedAmeaca = this.ameacas.find(ameaca => ameaca._id === this.tempDocument.ameaca);
-                        this.tempDocument.ameaca = selectedAmeaca;
-
-                        this.$emit('save', res.body || this.tempDocument)
-                    })
-            },
-            getProcessos() {
-                new Service(this.$resource('api/processos{/id}'))
-                    .findAll()
-                    .then(processos => this.processos = processos);
-            },
-            getAmeacas() {
-                new Service(this.$resource('api/ameacas{/id}'))
-                    .findAll()
-                    .then(ameacas => this.ameacas = ameacas);
-            }
+export default {
+    mixins: [CrudFormMixin],
+    props: {
+        document: {
+            required: true
         }
+    },
+    data() {
+        return {
+            processos: [],
+            ameacas: []
+        }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/acoes{/id}'))
+        this.serviceProcessos = new Service(this.$resource('api/processos{/id}'))
+        this.serviceAmeacas = new Service(this.$resource('api/ameacas{/id}'))
+    },
+    async mounted() {
+        this.processos = await this.serviceProcessos.findAll();
+        this.ameacas = await this.serviceAmeacas.findAll();
     }
+}
 </script>

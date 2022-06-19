dbac0edc4fe384e22c48efad664c161b0dc0a96f
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-19 11:39:40 +0100

    Applied Mixins

--- app/src/components/cadastros/CadastroProcesso.vue
+++ app/src/components/cadastros/CadastroProcesso.vue
@@ -1,94 +1,65 @@
 <template>
     <v-dialog v-model="isOpen" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">
+                Novo processo
+            </v-btn>
+        </template>
         <v-card>
             <v-card-title class="grey lighten-4 py-4 title">
-                {{ tempDocument._id ? 'Editar' : 'Novo' }} processo
+                {{ form._id ? 'Editar' : 'Novo' }} processo
             </v-card-title>
             <v-container grid-list-sm class="pa-4">
                 <v-layout row wrap>
                     <v-flex xs3>
-                        <v-text-field v-model="tempDocument._id" disabled label="Código"></v-text-field>
+                        <v-text-field v-model="form._id" disabled label="Código"></v-text-field>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-text-field v-model="tempDocument.descricao" label="Descrição"></v-text-field>
+                        <v-text-field v-model="form.descricao" label="Descrição"></v-text-field>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-autocomplete 
-                            :items="niveis" 
-                            v-model="tempDocument.nivel"
-                            label="Nível" 
-                            item-text="descricao" 
-                            item-value="_id" 
-                        ></v-autocomplete>
+                        <v-autocomplete :items="niveis" v-model="form.nivel" label="Nível" item-text="descricao"
+                            item-value="_id"></v-autocomplete>
                     </v-flex>
 
                     <v-flex xs12>
-                        <v-text-field v-model="tempDocument.tolerancia" label="Tolerância"></v-text-field>
+                        <v-text-field v-model="form.tolerancia" label="Tolerância"></v-text-field>
                     </v-flex>
                 </v-layout>
             </v-container>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="cancel()">Cancelar</v-btn>
-                <v-btn flat color="primary" @click="save()">Salvar</v-btn>
+                <v-btn @click="cancel()">Cancelar</v-btn>
+                <v-btn color="primary" @click="save()">Salvar</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
+import CrudFormMixin from '../../mixins/crud-form.mixin';
 
-    export default {
-        props: {
-            document: {
-                required: true
-            }
-        },
-        data() {
-            return {
-                isOpen: false,
-                tempDocument: {},
-                niveis: []
-            }
-        },
-        watch: {
-            document: function (newVal, oldVal) {
-                if (!this.document) {
-                    this.isOpen = false;
-                }
-                if (typeof this.document === 'object') {
-                    this.isOpen = true
-                    this.tempDocument = Object.assign({}, this.document)
-
-                    if (this.tempDocument.nivel) {
-                        this.tempDocument.nivel = this.tempDocument.nivel._id
-                    }
-
-                    this.getNiveis()
-                }
-            }
-        },
-        methods: {
-            cancel() {
-                this.$emit('cancel')
-            },
-            save() {
-                new Service(this.$resource('api/processos{/id}'))
-                    .save(this.tempDocument)
-                    .then((res) => {
-                        let selectedNivel = this.niveis.find(nivel => nivel._id === this.tempDocument.nivel);
-                        this.tempDocument.nivel = selectedNivel;
-                        this.$emit('save', res.body || this.tempDocument)
-                    })
-            },
-            getNiveis() {
-                new Service(this.$resource('api/niveis{/id}'))
-                    .findAll()
-                    .then(niveis => this.niveis = niveis);
-            }
+export default {
+    mixins: [CrudFormMixin],
+    props: {
+        document: {
+            required: true
         }
+    },
+    data() {
+        return {
+            niveis: []
+        }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/processos{/id}'))
+        this.serviceNiveis = new Service(this.$resource('api/niveis{/id}'))
+    },
+    async mounted() {
+        this.niveis = await this.serviceNiveis.findAll()
     }
+}
 </script>

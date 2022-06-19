ab39d9bd928bb413f26ac30983de8ac15509e17d
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-19 16:19:35 +0100

    Fixed layout of login

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -1,31 +1,27 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>BIA - Business Impact Analysis</v-toolbar-title>
-              </v-toolbar>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>BIA - Business Impact Analysis</v-toolbar-title>
+          </v-toolbar>
 
-              <v-card-text>
-                <v-form>
-                  <v-text-field v-model="document.login" prepend-icon="person" label="Usuário" type="text"></v-text-field>
-                  <v-text-field v-model="document.senha" prepend-icon="lock" label="Senha" type="password"></v-text-field>
-                </v-form>
-              </v-card-text>
+          <v-card-text>
+            <v-form>
+              <v-text-field v-model="form.login" prepend-icon="person" label="Usuário" type="text"></v-text-field>
+              <v-text-field v-model="form.senha" prepend-icon="lock" label="Senha" type="password"></v-text-field>
+            </v-form>
+          </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="entrar">Entrar</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="login">Entrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -34,16 +30,12 @@ import { AuthService } from "../domain/AuthService";
 export default {
   name: "Login",
   data: () => ({
-    document: {
-      login: "",
-      senha: ""
-    }
+    form: {}
   }),
   methods: {
-    entrar() {
-      new AuthService(this.$http).login(this.document).then(err => {
-        this.$router.push({ name: "app" });
-      }).catch((err) => {console.log(err)});
+    async login() {
+      await new AuthService(this.$http).login(this.form)
+      this.$router.push({ name: "home" });
     }
   }
 };

01551ce378985166ce0a78357cc204ddbaeff9fe
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-19 16:51:20 +0100

    Fixed layout

--- app/src/components/listas/AmeacasAosProcessos.vue
+++ app/src/components/listas/AmeacasAosProcessos.vue
@@ -1,55 +1,47 @@
 <template>
-    <div>
+    <v-container fluid>
         <v-toolbar flat color="white">
             <v-toolbar-title>Ameaças aos processos</v-toolbar-title>
         </v-toolbar>
 
-        <v-list two-line subheader>
-            <template v-for="(item, index) in items">
+        <v-list two-line subheader v-for="(item, index) in items" :key="index">
 
-                <v-divider :key="index"></v-divider>
+            <v-divider></v-divider>
 
-                <v-subheader
-                    v-if="item.descricao"
-                    :key="item._id"
-                >
-                    {{ item.descricao }}
-                </v-subheader>
+            <v-subheader v-if="item.descricao" :key="item._id">
+                {{ item.descricao }}
+            </v-subheader>
 
-                <template v-for="(acao) in item.acoes">
-                    <v-list-tile avatar :key="acao._id">
-
-                        <v-list-tile-content>
-                            <v-list-tile-title>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ acao.processo.descricao }} - {{ acao.processo.nivel.descricao }} - {{ acao.processo.tolerancia }}</v-list-tile-title>
-                            <!-- <v-list-tile-sub-title>{{ acao.processo.nivel.descricao }} - {{ acao.processo.tolerancia }}</v-list-tile-sub-title> -->
-                        </v-list-tile-content>
-
-                    </v-list-tile>
-                </template>
-
-            </template>
+            <v-list-item v-for="(acao) in item.acoes" :key="acao._id">
+                <v-list-item-content>
+                    <v-list-item-title>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ acao.processo.descricao }} - {{
+                            acao.processo.nivel.descricao
+                    }} - {{ acao.processo.tolerancia }}</v-list-item-title>
+                    <!-- <v-list-item-sub-title>{{ acao.processo.nivel.descricao }} - {{ acao.processo.tolerancia }}</v-list-item-sub-title> -->
+                </v-list-item-content>
+            </v-list-item>
 
         </v-list>
-    </div>
+    </v-container>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
 
-    export default {
-        name: 'ProcessosPorNivel',
-        data() {
-            return {
-                search: '',
-                items: []
-            }
-        },
-        created() {
-            this.service = new Service(this.$resource('api/ameacas-aos-processos{/id}'));
-
-            this.service
-                .findAll()
-                .then(items => this.items = items);
+export default {
+    name: 'ProcessosPorNivel',
+    data() {
+        return {
+            search: '',
+            items: []
         }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/ameacas-aos-processos{/id}'));
+
+        this.service
+            .findAll()
+            .then(items => this.items = items);
     }
+}
 </script>

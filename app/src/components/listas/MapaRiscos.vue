78af367c773fa7438e11828ce7cd3d95ec0b60c9
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-06-30 21:30:18 +0100

    Layout fixes

--- app/src/components/listas/MapaRiscos.vue
+++ app/src/components/listas/MapaRiscos.vue
@@ -1,63 +1,57 @@
 <template>
-    <div>
-        <v-toolbar flat color="white">
-            <v-toolbar-title>Mapa de riscos</v-toolbar-title>
-        </v-toolbar>
+    <v-container fluid>
 
-        <v-data-table hide-actions :headers="headers" :items="items">
-            <template slot="items" slot-scope="props">
-                <td>{{ props.item.descricao }}</td>
-                <td>{{ props.item.nivel && props.item.nivel.descricao }}</td>
-                <td>{{ props.item.tolerancia }}</td>
-                <td>
-                    <v-btn icon class="mx-0" @click="viewItem(props.item)">
-                        <v-icon color="blue">bug_report</v-icon>
-                    </v-btn>
-                </td>
+        <v-data-table hide-default-footer fix-header :headers="headers" :items="items">
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>Mapa de riscos</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>
+                </v-toolbar>
+            </template>
+
+            <template v-slot:item.actions="{ item }">
+                <v-icon small class="mr-2" @click="viewItem(item)">
+                    bug_report
+                </v-icon>
             </template>
         </v-data-table>
 
-        <VisualizarAmeacas
-            :document="visualizarAmeacas"
-            @close="visualizarAmeacas = null"
-        ></VisualizarAmeacas>
-    </div>
+        <VisualizarAmeacas :document="visualizarAmeacas" @close="visualizarAmeacas = null"></VisualizarAmeacas>
+    </v-container>
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
-    import DialogConfirmRemove from '../shared/DialogConfirmRemove'
-    import VisualizarAmeacas from '../cadastros/VisualizarAmeacas'
+import { Service } from '../../domain/Service'
+import VisualizarAmeacas from '../cadastros/VisualizarAmeacas'
 
-    export default {
-        name: 'MapaRiscos',
-        components: {
-            DialogConfirmRemove,
-            VisualizarAmeacas
-        },
-        data() {
-            return {
-                visualizarAmeacas: null,
-                headers: [
-                    { text: 'Processo', value: 'descricao' },
-                    { text: 'Nível', value: 'nivel.descricao' },
-                    { text: 'Tolerância', value: 'tolerancia' },
-                    { text: 'Ameaças', value: 'descricao', sortable: false }
-                ],
-                items: []
-            }
-        },
-        created() {
-            this.service = new Service(this.$resource('api/processos{/id}'));
+export default {
+    name: 'MapaRiscos',
+    components: {
+        VisualizarAmeacas
+    },
+    data() {
+        return {
+            visualizarAmeacas: null,
+            headers: [
+                { text: 'Processo', value: 'descricao' },
+                { text: 'Nível', value: 'nivel.descricao' },
+                { text: 'Tolerância', value: 'tolerancia' },
+                { text: 'Ameaças', value: 'actions', sortable: false, align: 'center' }
+            ],
+            items: []
+        }
+    },
+    created() {
+        this.service = new Service(this.$resource('api/processos{/id}'));
 
-            this.service
-                .findAll()
-                .then(items => this.items = items);
-        },
-        methods: {
-            viewItem(item) {
-                this.visualizarAmeacas = item
-            }
+        this.service
+            .findAll()
+            .then(items => this.items = items);
+    },
+    methods: {
+        viewItem(item) {
+            this.visualizarAmeacas = item
         }
     }
+}
 </script>

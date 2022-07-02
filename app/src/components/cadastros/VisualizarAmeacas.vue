c309b544f46d2315340e8e9b773481b52568f5ea
Gabriel Arsenio <gabrielarsenioo@gmail.com>
2022-07-02 23:17:21 +0100

    Fixxed migration bugs

--- app/src/components/cadastros/VisualizarAmeacas.vue
+++ app/src/components/cadastros/VisualizarAmeacas.vue
@@ -4,7 +4,7 @@
             <v-card-title class="grey lighten-4 py-4 title">
                 Visualizar ameaças
                 <v-spacer></v-spacer>
-                <v-btn flat icon color="red" @click="$emit('close')">
+                <v-btn icon color="red" @click="$emit('close')">
                     <v-icon>close</v-icon>
                 </v-btn>
             </v-card-title>
@@ -12,11 +12,7 @@
             <v-container grid-list-lg>
                 <v-layout row wrap>
                     <v-flex xs12>
-                        <v-data-table hide-actions fix-header :headers="headers" :items="acoes">
-                            <template slot="items" slot-scope="props">
-                                <td>{{ props.item.ameaca.descricao }}</td>
-                                <td>{{ props.item.procedimento }}</td>
-                            </template>
+                        <v-data-table hide-default-footer fix-header :headers="headers" :items="acoes">
                         </v-data-table>
                     </v-flex>
                 </v-layout>
@@ -24,7 +20,7 @@
 
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="$emit('close')">Fechar</v-btn>
+                <v-btn @click="$emit('close')">Fechar</v-btn>
             </v-card-actions>
 
         </v-card>
@@ -32,52 +28,52 @@
 </template>
 
 <script>
-    import { Service } from '../../domain/Service'
+import { Service } from '../../domain/Service'
 
-    export default {
-        props: {
-            document: {
-                required: true
-            }
-        },        
-        data() {
-            return {
-                isOpen: false,
-                headers: [
-                    { text: 'Ameaça', value: 'descricao' },
-                    { text: 'Indicativo de ação', value: 'descricao' }
-                ],
-                acoes: [],
-                tempDocument: { 
-                    acao:{
-                        processo: { nivel: {} },
-                        ameaca: {}
-                    }
-                } 
-            }
-        },
-        watch: {
-            document: function(newVal) {
-                this.isOpen = !!newVal
-
-                if (!this.document) {
-                    return
+export default {
+    props: {
+        document: {
+            required: true
+        }
+    },
+    data() {
+        return {
+            isOpen: false,
+            headers: [
+                { text: 'Ameaça', value: 'ameaca.descricao' },
+                { text: 'Indicativo de ação', value: 'procedimento' }
+            ],
+            acoes: [],
+            tempDocument: {
+                acao: {
+                    processo: { nivel: {} },
+                    ameaca: {}
                 }
-
-                this.tempDocument = Object.assign({}, newVal)
-                this.loadAmeacas();
-            }
-        },
-        methods: {
-            loadAmeacas() {
-                this.service = new Service(this.$resource('api/acoes{/id}'));
-
-                this.service
-                    .findAll()
-                    .then(items => {
-                        this.acoes = items.filter(item => item.processo._id === this.tempDocument._id)
-                    });
             }
         }
+    },
+    watch: {
+        document: function (newVal) {
+            this.isOpen = !!newVal
+
+            if (!this.document) {
+                return
+            }
+
+            this.tempDocument = Object.assign({}, newVal)
+            this.loadAmeacas();
+        }
+    },
+    methods: {
+        loadAmeacas() {
+            this.service = new Service(this.$resource('api/acoes{/id}'));
+
+            this.service
+                .findAll()
+                .then(items => {
+                    this.acoes = items.filter(item => item.processo._id === this.tempDocument._id)
+                });
+        }
     }
+}
 </script>
